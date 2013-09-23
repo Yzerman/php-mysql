33eca3f9acaad0e08e5c1a8317b75834523e2872
Patrice Keusch <yzerman.detroit@bluewin.ch>
2013-09-23 17:31:48 +0200

    homework 3 v2

--- dev1/web/php/mailform.php
+++ dev1/web/php/mailform.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 echo'
 	<form method="post" action="php/sendmail.php">
@@ -16,4 +17,24 @@ echo'
     <input id="submit" name="submit" type="submit" value="Submit">
         
 	</form>'
+=======
+<?php
+echo'
+	<form method="post" action="php/sendmail.php">
+        
+    <label>Name</label>
+    <input name="name" placeholder="Type Here">
+            
+    <label>Email</label>
+    <input name="email" type="email" placeholder="Type Here">
+            
+    <label>Message</label>
+    <textarea name="message" placeholder="Type Here"></textarea>
+	<label>*What is 2+2? (Anti-spam)</label>
+	<input name="human" placeholder="Type Here">
+            
+    <input id="submit" name="submit" type="submit" value="Submit">
+        
+	</form>'
+>>>>>>> 74ba8cdb0ae82bd05e9c970edde3b2661e73b302
 ?>
--- dev1/web/php/test.php
+++ dev1/web/php/test.php
@@ -1,3 +1,36 @@
+<<<<<<< HEAD
+<?php
+ini_set('display_errors', '1');
+error_reporting(E_ALL);
+$empfaenger = 'yzerman.detroit@bluewin.ch';
+$betreff = 'Uebung3 - PHP-Kurs';
+$nachricht = 'Hallo - Das ist die Uebungs-Nachricht';
+$header = 'From: yzerman.detroit@bluewin.ch' . "\r\n" .
+    'Reply-To: yzerman.detroit@bluewin.ch' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+
+mail($empfaenger, $betreff, $nachricht, $header);
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Email-Send</title>
+	</head>
+			
+	<body>
+		<p>Das Mail wurde eventuell gesendet - wenn php so wollte.</p>
+	</body>
+</html>
+
+=======
 <?php
 ini_set('display_errors', '1');
 error_reporting(E_ALL);
@@ -31,3 +64,4 @@ mail($empfaenger, $betreff, $nachricht, $header);
 	</body>
 </html>
 
+>>>>>>> 74ba8cdb0ae82bd05e9c970edde3b2661e73b302
