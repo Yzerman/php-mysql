eb9b96bf90bc16a9fe7f7896913dbcfd54a1dd0a
Patrice Keusch <yzerman.detroit@bluewin.ch>
2013-11-25 18:19:16 +0100

    yii Kapitel 1-5

---
+++ yii/framework/helloworld/protected/views/message/goodbye.php
@@ -0,0 +1,5 @@
+<h1> Goodbye, Yii developer!</h1>
+<p> 
+<?php echo CHtml::link('Hello',array('message/hello')); 
+    ?>
+</p>
---
+++ yii/framework/helloworld/protected/views/message/hello.php
@@ -0,0 +1,8 @@
+<?php
+?><h1> Hello World! </h1>
+<h3><?php echo $time; ?></h3>
+<p> 
+    <?php echo CHtml::link('Goodbye',array('message/goodbye')); 
+    ?>
+</p>
+
